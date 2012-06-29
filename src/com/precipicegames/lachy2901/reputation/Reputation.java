62d471470c3890d66e6b5ff23e269d8069f77dbb
Lachlan H <lachy2901@gmail.com>
2012-06-29 09:59:42 +0930

    Initial Commit
    A simple plugin skeleton.

---
+++ src/com/precipicegames/lachy2901/reputation/Reputation.java
@@ -0,0 +1,21 @@
+package com.precipicegames.lachy2901.reputation;
+
+import org.bukkit.command.*;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Reputation extends JavaPlugin {
+
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public boolean onCommand(CommandSender cs, Command cmd, String cl, String[] a) {
+		
+		return true;
+	}
+	
+}
